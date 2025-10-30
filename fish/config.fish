d9376af7e3570015bc076438d2b693ed3d6c286b
Łaurent ʘ❢Ŧ Ŧough <LaurentFough@users.noreply.github.com>
2025-10-29 22:23:55 -0700

    Rename activity file and watcher binary variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 #= TERMTTY_IDLE Activity Watcher
 set -g -x TERMTTY_IDLE_ONCE 0
 set -g -x TERMTTY_IDLE_CMD '$XDG_CONFIG_HOME/bash/screensavers/screensaver.sh 6'
-set -g -x TERMTTY_IDLE_ACTIVITY_FILE $HOME/.termtty_idle_watch_activity.( id -u 2>/dev/null || echo $fish_pid )
-set -g -x TERMTTY_IDLE_WATCH_BIN $XDG_BIN_HOME/termtty_idle_watch_core.sh
+set -g -x TERMTTY_IDLE_ACTIVITY_FILE $HOME/.termtty_idle_watcher_activity.( id -u 2>/dev/null || echo $fish_pid )
+set -g -x TERMTTY_IDLE_WATCHER_BIN $XDG_BIN_HOME/termtty_idle_watcher_core.sh
